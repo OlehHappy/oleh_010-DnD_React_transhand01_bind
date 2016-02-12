5a03a8e2a25633f540b67aa5cd83e32b227a41cb
Oleh Daybov <olegtranslator@gmail.com>
2016-02-12 20:49:15 +0200

    working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// require('./index.html')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import scatterThings from './prepare'
+import App from './App'
+
+scatterThings()
+
+ReactDOM.render(<App/>, document.querySelector('#mount-app'))
